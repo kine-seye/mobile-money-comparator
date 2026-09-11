000edb0c4822f33be33e35084a7c1df2298ea817
Kine Seye <kineseyeetudiante448@gmail.com>
2026-09-11 17:52:11 +0000

    Ajout du script de peuplement des donnees de reference

---
+++ db/seed_reference_data.sql
@@ -0,0 +1,13 @@
+-- insertion des opérateur eu sénégal
+insert into operateur(nom) values
+('Wave'),
+('Orange money'),
+('Free money');
+
+
+-- insertion des types d'opérateur de base
+insert into type_operation(nom) values
+('Retrait'),
+('Depot'),
+('Transfert'),
+('Paiment Marchand');
