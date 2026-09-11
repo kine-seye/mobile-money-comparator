4532c8a416e488989901fe6359687ddae26623a7
Kine Seye <kineseyeetudiante448@gmail.com>
2026-09-11 17:42:45 +0000

    Finalisation propre du schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+--table 1 : les opérateurs
+create table operateur(
+	id serial primary key,
+	nom varchar(50) not null unique
+); 
+-- les types d'opération
+create table type_operation(
+	id serial primary key,
+	nom varchar(50) not null unique
+);
+
+-- les tranches de frias
+create table grille_tarifaire(
+	id serial primary key,
+	operateur_id int references operateur(id),
+	type_operation_id int references type_operation(id),
+	montant_min numeric(10,2) not null,
+	montant_max numeric(10,2),
+	frais_fixe numeric(10,2) default 0,
+	frais_pourcentage numeric (5,4) default 0,
+	dat_j date not null default current_date
+);
+
+	
+	
